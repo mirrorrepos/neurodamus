492667c1856446957389a426da5ef187eeb8aadd
Jorge Blanco Alonso <jorge.blancoalonso@epfl.ch>
2023-06-02 14:15:04 +0200

    Add lfp test for v5_sonata simulation
    LIBSONATA_BRANCH=jblanco/lfp_electrodes_file

--- tests/test_lfp.py
+++ tests/test_lfp.py
@@ -1,5 +1,19 @@
 import pytest
 import h5py
+import os
+import numpy as np
+from pathlib import Path
+
+SIM_DIR = Path(__file__).parent.absolute() / "simulations"
+
+pytestmark = [
+    pytest.mark.forked,
+    pytest.mark.slow,
+    pytest.mark.skipif(
+        not os.environ.get("NEURODAMUS_NEOCORTEX_ROOT"),
+        reason="Test requires loading a neocortex model to run"
+    )
+]
 
 
 @pytest.fixture
@@ -11,12 +25,12 @@ def test_file(tmpdir):
     test_file = h5py.File(tmpdir.join("test_file.h5"), 'w')
     test_file.create_group("electrodes")
     test_file["electrodes"].create_group("electrode_grid")
-    test_file["electrodes"]["electrode_grid"].create_dataset("0", data=[[0.1, 0.2], [0.3, 0.4]])
-    test_file["electrodes"]["electrode_grid"].create_dataset("1", data=[[0.5, 0.6], [0.7, 0.8]])
+    test_file["electrodes"]["electrode_grid"].create_dataset("62798", data=[[0.1, 0.2], [0.3, 0.4]])
+    test_file["electrodes"]["electrode_grid"].create_dataset("63699", data=[[0.5, 0.6], [0.7, 0.8]])
     test_file.create_group("sec_ids")
-    test_file["sec_ids"].create_dataset("0", data=[0, 1])
-    test_file["sec_ids"].create_dataset("1", data=[2, 3])
-    node_ids = test_file.create_dataset("node_ids", data=[0, 1])
+    test_file["sec_ids"].create_dataset("62798", data=[0, 1])
+    test_file["sec_ids"].create_dataset("63699", data=[1, 2])
+    node_ids = test_file.create_dataset("node_ids", data=[62798, 63699])
     node_ids.attrs["circuit"] = "test_circuit.h5"
     yield test_file
 
@@ -75,7 +89,7 @@ def test_read_lfp_factors(test_file):
     lfp = LFPManager()
     lfp._lfp_file = test_file
     # Test the function with valid input
-    gid = 0
+    gid = 62798
     section_ids = [1, 2]
     result = lfp.read_lfp_factors(gid, section_ids).to_python()
     expected_result = [0.3, 0.4]
@@ -99,7 +113,7 @@ def test_number_electrodes(test_file):
     lfp = LFPManager()
     lfp._lfp_file = test_file
     # Test the function with valid input
-    gid = 0
+    gid = 62798
     result = lfp.get_number_electrodes(gid)
     expected_result = 2
     assert result == expected_result, f'Expected {expected_result}, but got {result}'
@@ -109,3 +123,59 @@ def test_number_electrodes(test_file):
     result = lfp.get_number_electrodes(gid)
     expected_result = 0
     assert result == expected_result, f'Expected {expected_result}, but got {result}'
+
+
+def _create_tmpconfig_lfp(config_file, lfp_file):
+    import fileinput
+    import shutil
+    from tempfile import NamedTemporaryFile
+
+    lfp_replace = "\"electrodes_file\": \"" + str(lfp_file) + "\""
+    suffix = ".json" if config_file.endswith(".json") else ".BC"
+    tmp_file = NamedTemporaryFile(suffix=suffix, dir=os.path.dirname(config_file), delete=True)
+    shutil.copy2(config_file, tmp_file.name)
+
+    with fileinput.FileInput(tmp_file.name, inplace=True) as file:
+        for line in file:
+            if config_file.endswith(".json"):
+                print(line.replace("\"electrodes_file\": \"electrodes_file.h5\"",
+                                   lfp_replace), end='')
+    return tmp_file
+
+
+def _read_sonata_lfp_file(lfp_file):
+    import libsonata
+    report = libsonata.ElementReportReader(lfp_file)
+    pop_name = report.get_population_names()[0]
+    node_ids = report[pop_name].get_node_ids()
+    data = report[pop_name].get()
+    return node_ids, data
+
+
+def test_v5_sonata_lfp(tmpdir, test_file):
+    import numpy.testing as npt
+    from neurodamus import Neurodamus
+
+    config_file = str(SIM_DIR / "v5_sonata" / "simulation_config_lfp.json")
+    output_dir = str(SIM_DIR / "v5_sonata" / "output_coreneuron")
+
+    test_file["node_ids"].attrs['circuit'] = "/gpfs/bbp.cscs.ch/project/proj1/circuits/" \
+                                             "SomatosensoryCxS1-v5.r0/O1-sonata/sonata/networks" \
+                                             "/nodes/default/nodes.h5"
+    lfp_weights_file = tmpdir.join("test_file.h5")
+    tmp_file = _create_tmpconfig_lfp(config_file, lfp_weights_file)
+
+    nd = Neurodamus(tmp_file.name, output_path=output_dir)
+    nd.run()
+
+    # compare results with refs
+    t3_data = np.array([-7.392764091491699219e-04, -1.529844943434000015e-03,
+                        9.888911154121160507e-04, 1.153199234977364540e-03])
+    t7_data = np.array([-7.781040039844810963e-04, -1.588534913025796413e-03,
+                        7.941523799672722816e-04, 9.259916841983795166e-04])
+    node_ids = np.array([62797, 63698])
+    result_ids, result_data = _read_sonata_lfp_file(os.path.join(output_dir, "lfp.h5"))
+
+    npt.assert_allclose(result_data.data[3], t3_data)
+    npt.assert_allclose(result_data.data[7], t7_data)
+    npt.assert_allclose(result_ids, node_ids)
